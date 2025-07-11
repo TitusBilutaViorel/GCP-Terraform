52397965db37460b2899f9ae687867c2c09d63bc
TitusViorelBiluta <titusbiluta@gmail.com>
2025-07-11 13:02:05 +0300

    chore: edit main.py for testing on path

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello from Cloud Run. Changed by Titus!'
+    return 'Hello from Cloud Run'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
